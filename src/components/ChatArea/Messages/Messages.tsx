81d6c6ce0d3b2408afe725f740c1a7edfda583c1
ansub <khanansub3@gmail.com>
2025-02-04 18:29:44 +0400

    refactor: Improve UserMessage and AgentMessage component styling

--- src/components/ChatArea/Messages/Messages.tsx
+++ src/components/ChatArea/Messages/Messages.tsx
@@ -1,67 +1,60 @@
-import { memo } from 'react'
+import { memo } from "react";
 
 // import Icon from '@/components/ui/icon'
 
-import MarkdownRenderer from '@/components/ui/typography/MarkdownRenderer'
-import { usePlaygroundStore } from '@/stores/PlaygroundStore'
+import MarkdownRenderer from "@/components/ui/typography/MarkdownRenderer";
+import { usePlaygroundStore } from "@/stores/PlaygroundStore";
 
-import type { PlaygroundChatMessage } from '@/types/playground'
-import AgentThinkingLoader from '../AgentThinkingLoader'
+import type { PlaygroundChatMessage } from "@/types/playground";
+import AgentThinkingLoader from "../AgentThinkingLoader";
 
 interface MessageProps {
-  message: PlaygroundChatMessage
+  message: PlaygroundChatMessage;
 }
 
 export const AgentMessage = ({ message }: MessageProps) => {
-  const { streamingError } = usePlaygroundStore()
+  const { streamingError } = usePlaygroundStore();
 
-  let messageContent
+  let messageContent;
   if (message.content) {
     messageContent = (
       <div className="flex w-full flex-col gap-4">
         <MarkdownRenderer>{message.content}</MarkdownRenderer>
       </div>
-    )
+    );
   } else if (streamingError) {
     messageContent = (
       <p className="text-destructive">
         Oops! Something went wrong with the stream. Please try again, or refresh
         the page.
       </p>
-    )
+    );
   } else {
     messageContent = (
       <div className="mt-2 flex items-start">
         <AgentThinkingLoader />
       </div>
-    )
+    );
   }
 
   return (
     <div className="flex flex-row items-start gap-4">
       <div className="shrink-0">
-        <div>AI : </div>
+        <div>AI</div>
       </div>
       {messageContent}
     </div>
-  )
-}
+  );
+};
 
 export const UserMessage = memo(({ message }: MessageProps) => {
-  const username = "U"
-
   return (
-    <div className="flex flex-row items-start gap-4 pt-4 text-start max-md:break-words">
-      <div className="mt-1 flex size-6 shrink-0 items-center justify-center rounded-sm bg-brand text-[10px]">
-        <div className="text-white uppercase">
-          {username}
-        </div>
-      </div>
-      <div className="mt-0.1 text-lg font-medium text-primary/80">
+    <div className="flex flex-row-reverse items-start gap-4 pt-4 text-start max-md:break-words">
+      <div className="text-md font-medium text-primary/80 bg-secondary px-2 py-1 rounded-lg">
         {message.content}
       </div>
     </div>
-  )
-})
+  );
+});
 
-UserMessage.displayName = 'UserMessage'
+UserMessage.displayName = "UserMessage";
