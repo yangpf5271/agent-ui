80a93103ecd0e8298125f9560c976334063d2f87
ansub <khanansub3@gmail.com>
2025-02-06 12:40:41 +0400

    feat: Add agent icon and improve agent selector and message styling

--- src/components/ChatArea/Messages/Messages.tsx
+++ src/components/ChatArea/Messages/Messages.tsx
@@ -1,18 +1,17 @@
 import { memo } from "react";
-import { UserIcon } from "@/components/ui/Icons";
+import { AgentIcon, UserIcon } from "@/components/ui/Icons";
 
 import MarkdownRenderer from "@/components/ui/typography/MarkdownRenderer";
 import { usePlaygroundStore } from "@/stores/PlaygroundStore";
 
 import type { PlaygroundChatMessage } from "@/types/playground";
-import { Sparkles } from "lucide-react";
 
 interface MessageProps {
   message: PlaygroundChatMessage;
 }
 
 export const AgentMessage = ({ message }: MessageProps) => {
-  const { streamingError } = usePlaygroundStore();
+  const { streamingError, selectedAgent } = usePlaygroundStore();
 
   let messageContent;
   if (message.content) {
@@ -35,11 +34,9 @@ export const AgentMessage = ({ message }: MessageProps) => {
   }
 
   return (
-    <div className="flex flex-row items-start gap-4">
-      <div className="shrink-0">
-        <div className="bg-brand text-secondary rounded-full p-1">
-          <Sparkles size={16} />
-        </div>
+    <div className="flex flex-col items-start gap-4 font-inter">
+      <div className="flex items-center gap-x-2 text-sm font-medium font-geist uppercase text-brand">
+        <AgentIcon /> {selectedAgent}
       </div>
       {messageContent}
     </div>
@@ -50,10 +47,10 @@ export const UserMessage = memo(({ message }: MessageProps) => {
   return (
     <div className="flex items-start pt-4 text-start max-md:break-words">
       <div className="flex flex-col gap-y-3">
-        <p className="text-muted flex items-center gap-x-2 text-xs font-medium">
+        <p className="text-muted flex items-center gap-x-2 text-sm font-medium">
           <UserIcon /> <span className="uppercase">you</span>{" "}
         </p>
-        <div className="text-sm text-secondary py-1 rounded-lg">
+        <div className="text-md text-secondary py-1 rounded-lg font-inter">
           {message.content}
         </div>
       </div>
