ab74c2aa546fc6be1b882ea1340b09244cc45474
ansub <khanansub3@gmail.com>
2025-02-06 11:47:56 +0400

    style: Refine dark mode UI with enhanced background colors and rounded elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <main className="relative flex flex-col flex-grow">
+      <main className="relative flex flex-col flex-grow bg-[#111113] rounded-xl m-1.5">
         <ChatArea />
         <div className="sticky bottom-0 px-4 pb-2">
           <ChatInput />
