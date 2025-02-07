4acf32b55a5451a2f260e206221de7c9f0f7aaa2
anuragts <anuragsharma011011@gmail.com>
2025-02-07 17:44:40 +0530

    fix: scroll-to-bottom and fix scroll issues

--- src/components/ChatArea/ChatArea.tsx
+++ src/components/ChatArea/ChatArea.tsx
@@ -2,17 +2,26 @@
 
 import { usePlaygroundStore } from "@/stores/PlaygroundStore";
 import MessageList from "./Messages/MessageList";
+import ScrollToBottomButton from "@/components/ChatArea/ScrollToBottom";
+import { StickToBottom } from "use-stick-to-bottom";
+
 const ChatArea = () => {
   const { messages } = usePlaygroundStore();
 
   return (
-    <div className="relative flex min-h-0 flex-grow flex-col overflow-y-auto rounded-lg">
-      <div className="flex min-h-full flex-col justify-center">
+    <StickToBottom
+      className="relative mb-4 flex max-h-[calc(100vh-64px)] min-h-0 flex-grow flex-col"
+      resize="smooth"
+      initial="smooth"
+    >
+      {" "}
+      <StickToBottom.Content className="flex min-h-full flex-col justify-center">
         <div className="mx-auto w-full max-w-2xl space-y-9 px-4 pb-4">
           <MessageList messages={messages} />
         </div>
-      </div>
-    </div>
+      </StickToBottom.Content>
+      <ScrollToBottomButton />
+    </StickToBottom>
   );
 };
 
