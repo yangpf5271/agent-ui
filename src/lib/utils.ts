80ebd223b231e283054e0237b774523a77a8c256
anuragts <anuragsharma011011@gmail.com>
2025-03-25 16:54:08 +0530

    fix: show 2 dots

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const truncateText = (text: string, limit: number) => {
   if (text) {
-    return text.length > limit ? `${text.slice(0, limit)}...` : text
+    return text.length > limit ? `${text.slice(0, limit)}..` : text
   }
   return ''
 }
